0ebd2b2cdc1bf7e43ce1b89b6a9879f135ef9d5c
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-01-15 16:44:52 +0100

    start implementing home-manager module

--- flake.nix
+++ flake.nix
@@ -34,7 +34,8 @@
           ) config.sops.secrets;
 
           mirageScript = pkgs.writeShellScript "mirage-dynamic-service" ''
-            #!/usr/bin/env bash
+
+            echo "Searching for files containing a mirage placeholder..."
 
             files=()
             while read -r path; do
@@ -80,6 +81,79 @@
 
               bindsTo = [ "sysinit-reactivation.target" ];
               partOf = [ "sysinit-reactivation.target" ];
+
+              serviceConfig = {
+                ExecStart = "${mirageScript}";
+                Restart = "always";
+              };
+            };
+
+            sops.mirage.placeholder = mapAttrs (
+              name: _: mkDefault "<MIRAGE:${builtins.hashString "sha256" name}:MIRAGE_PLACEHOLDER>"
+            ) config.sops.secrets;
+          };
+        };
+
+      homeManagerModules.mirage =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        with lib;
+        let
+          rgCommand = "${pkgs.ripgrep}/bin/rg -L -l --no-messages 'MIRAGE_PLACEHOLDER' ~/.local/state/nix/profiles/";
+
+          mirageArgs = mapAttrsToList (
+            name: value: "${config.sops.mirage.placeholder.${name}}=cat ${value.path}"
+          ) config.sops.secrets;
+
+          mirageScript = pkgs.writeShellScript "mirage-dynamic-service" ''
+
+            echo "Searching for files containing a mirage placeholder..."
+
+            files=()
+            while read -r path; do
+              resolved_path=$(readlink -f "$path")
+              echo "Found file: $resolved_path"
+              files+=("$resolved_path")
+            done < <(${rgCommand})
+
+            if [ ''${#files[@]} -eq 0 ]; then
+              echo "No files found. Sleeping forever..."
+              sleep infinity
+            fi
+
+            echo "Starting Mirage for files: ''${files[@]}"
+            # ${mirage.defaultPackage.${pkgs.system}}/bin/mirage "''${files[@]}" \
+            #   --shell ${pkgs.bash}/bin/sh \
+            #   ${lib.concatMapStringsSep " " (r: "--replace-exec '" + r + "'") mirageArgs} \
+            #   --allow-other
+          '';
+        in
+        {
+          options.sops.mirage = {
+            enable = mkEnableOption "Enable the sops mirage service";
+
+            placeholder = mkOption {
+              type = types.attrsOf (
+                types.mkOptionType {
+                  name = "coercibleToString";
+                  description = "value that can be coerced to string";
+                  check = lib.strings.isConvertibleWithToString;
+                  merge = lib.mergeEqualOption;
+                }
+              );
+              default = { };
+              visible = false;
+            };
+          };
+
+          config = {
+            systemd.user.services.mirage = {
+              description = "Mirage Service with dynamic file detection";
+              wantedBy = [ "default.target" ];
 
               serviceConfig = {
                 ExecStart = "${mirageScript}";
